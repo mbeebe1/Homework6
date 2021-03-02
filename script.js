0a89edb520c532597bb9e47813fd5615c09171e1
Michael Beebe <beebe79@gmail.com>
2021-03-02 17:09:19 -0500

    expanded html and styled headings into their places

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
- var requestUrl = 'https://openweathermap.org/api'
+//  var requestUrl = 'https://openweathermap.org/api'
+var requestUrl= 'https://api.openweathermap.org/data/2.5/forecast?q={cityname}&appid=e4fbd9e55ce3881a99f08e077864dcc3'
